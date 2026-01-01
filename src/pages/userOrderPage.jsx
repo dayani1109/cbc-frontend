4e3673404854eedbaece605ae4b1e9407c110b62
dayani <mkdkdesilva@gmail.com>
2026-01-01 11:29:42 +0530

    Footer Header Problem Clear

--- src/pages/userOrderPage.jsx
+++ src/pages/userOrderPage.jsx
@@ -3,9 +3,9 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import jsPDF from "jspdf";
 
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Footer from "../components/footer";
 import { Loder } from "../components/loder";
+import Header from "../components/header";
 
 export default function UserOrdersPage() {
   const [orders, setOrders] = useState([]);

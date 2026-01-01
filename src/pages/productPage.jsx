cb2de004fc2825dd67eed3f12b6888c72c1f4858
dayani <mkdkdesilva@gmail.com>
2026-01-01 09:36:11 +0530

    Here Update User settings and Search also  User Order Page Create 💕👍

--- src/pages/productPage.jsx
+++ src/pages/productPage.jsx
@@ -3,7 +3,8 @@ import toast from "react-hot-toast";
 import { Loder } from "../components/loder";
 import ProductCard from "../components/productCard";
 import axios from "axios";
-import Footer from "../components/footer";
+import Footer from "../components/Footer";
+import Header from "../components/header";
 
 export default function ProductPage() {
   const [products, setProducts] = useState([]); //me usestate 2 aniwa ona
@@ -27,6 +28,32 @@ export default function ProductPage() {
 
   return (
     <div className="w-full min-h-[calc(100vh-100px)] bg-primary">
+      <Header /> 
+      <div className="w-full h-[100px] flex justify-center items-center ">
+        <input
+          type="text"
+          onChange={async (e) => {
+            try {
+              if (e.target.value == "") {
+                setIsLoading(true);
+              } else {
+                const searchResult = await axios.get(
+                  import.meta.env.VITE_API_URL +
+                    "/api/products/search/" +
+                    e.target.value
+                );
+                setProducts(searchResult.data); //result data tika product vidiyt set karagannva
+                setIsLoading(false);
+              }
+            } catch (error) {
+              console.error(error.response?.data || error.message);
+              toast.error("search failed");
+            }
+          }} //value change vena hema velavkm
+          placeholder="Search Products.."
+          className="px-4 py-2 rounded-lg border border-secondary/10 bg-white/80"
+        ></input>
+      </div>
       {isLoading ? (
         <Loder />
       ) : (
@@ -36,7 +63,7 @@ export default function ProductPage() {
           })}
         </div>
       )}
-      <Footer/>
+      <Footer />
     </div>
   );
 }
